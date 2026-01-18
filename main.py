1613ea920bf722f3d1d357b036073a10c16219d8
Astitva <maurya.astitva@gmail.com>
2026-01-18 19:11:11 +0530

    Add sentry debug route

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 import sentry_sdk
 
 sentry_sdk.init(
-    dsn="https://911b0f938801e4bd64ff19981af1cf2f@o4510731654397952.ingest.us.sentry.io/4510731655577600",
+    dsn="https://2f070d8a068deb7da71b50b48bad718e@o4510731654397952.ingest.us.sentry.io/4510731685396480",
     # Add data like request headers and IP for users,
     # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     send_default_pii=True,
@@ -18,3 +18,6 @@ def home():
 def error():
     x = None
     return x.id   # ❌ This will crash (intentional)
+@app.get("/sentry-debug")
+async def trigger_error():
+    division_by_zero = 1 / 0
