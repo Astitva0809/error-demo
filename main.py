5a27a9de892a00d8cf09fe5a1fa747132e188208
Astitva <maurya.astitva@gmail.com>
2026-01-18 19:01:56 +0530

    Add Sentry error tracking

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://911b0f938801e4bd64ff19981af1cf2f@o4510731654397952.ingest.us.sentry.io/4510731655577600",
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+)
 
 app = FastAPI()
 
